7ad04b1c8e2c086263bc1c8267425d454ebd0080
Sviatoslav <svyatoslav200104@gmail.com>
2024-12-16 15:29:56 +0200

    Update userApi.ts

--- shared/services/userApi.ts
+++ shared/services/userApi.ts
@@ -1,9 +1,61 @@
 import axios from "axios";
 import { axiosInstance } from "./axiosInstance";
+import { Role } from "@/types";
 
 export const createUser = async () => {};
-export const updateUser = async () => {};
-export const deleteUser = async () => {};
+export const updateUser = async (id: string, updateUserDto: {
+   fullname?: string,
+  email?: string,  
+  password?: string,
+  phone?: string,
+  bio?: string,
+  role?: Role,
+  resetToken?: string,
+  resetTokenExp?: Date,
+  isVerified?: boolean,
+  isBlocked?: boolean,
+}) => {
+  try {
+    const response = await axiosInstance.patch("/users/" + id, { ...updateUserDto });
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+     if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const deleteUser = async (idOrEmail: string) => {
+    try {
+    const response = await axiosInstance.delete("/users/" + idOrEmail);
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+     if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
 export const getMe = async () => {
      try {
     const response = await axiosInstance.get("/users/me");
@@ -26,5 +78,51 @@ export const getMe = async () => {
     };
   }
 };
-export const fetchUsers = async () => {};
-export const fetchUserByIdOrEmail = async () => {};
+export const fetchUsers = async ({
+  page = 1,
+  perPage = 10,
+}: {
+  page: number;
+  perPage: number;
+}) => {
+  try {
+    const response = await axiosInstance.get("/users?page=" + page + "&perPage=" + perPage);
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+     if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const fetchUserByIdOrEmail = async (idOrEmail: string ) => {
+  try {
+    const response = await axiosInstance.get("/users/" + idOrEmail);
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+     if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
