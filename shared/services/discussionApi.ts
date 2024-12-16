9750fb82d929acb9cdb49928e1828f823130ecb8
Sviatoslav <svyatoslav200104@gmail.com>
2024-12-16 17:32:36 +0200

    Updated services

---
+++ shared/services/discussionApi.ts
@@ -0,0 +1,266 @@
+import axios from "axios";
+import { axiosInstance } from "./axiosInstance";
+import { Discussion, Opinion } from "@/types";
+
+export interface DiscussionParameters {
+  per_page: string;
+  page: string;
+  search: string;
+  sortBy: "title" | "date";
+  order: "asc" | "desc";
+  authorId: string;
+}
+export interface OpinionParameters {
+  per_page: string;
+  page: string;
+  discussionId: string;
+  order: "asc" | "desc";
+  authorId: string;
+}
+
+export const createDiscussion = async (
+  dto: Pick<Discussion, "title" | "content">
+) => {
+  try {
+    const response = await axiosInstance.post("discussions", {
+      title: dto.title,
+      content: dto.content ? dto.content : "",
+    });
+
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const updateDiscussion = async (dto: Partial<Discussion>) => {
+  try {
+    const response = await axiosInstance.put(`discussions/${dto.id}`, {
+      ...dto,
+    });
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const deleteDiscussion = async (id: string) => {
+  try {
+    const res = await axiosInstance.delete("discussions/" + id);
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+
+export const fetchDiscussions = async ({
+  per_page = "10",
+  page = "1",
+  search = "",
+  sortBy = "title",
+  order = "asc",
+  authorId = "",
+}: DiscussionParameters) => {
+  try {
+    const res = await axiosInstance.get(
+      `discussions?per_page=${per_page}&page=${page}&sortBy=${sortBy}&order=${order}&author-id=${authorId}&search=${search}`
+    );
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const fetchDiscussionById = async (id: string) => {
+  try {
+    const res = await axiosInstance.get(`discussions/${id}`);
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+
+export const createOpinion = async (dto: {
+  discussionId: string;
+  content: string;
+}) => {
+  try {
+    const response = await axiosInstance.post("opinions", {
+      ...dto,
+    });
+
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const updateOpinion = async (dto: Partial<Opinion>) => {
+  try {
+    const response = await axiosInstance.put(`opinions/${dto.id}`, {
+      ...dto,
+    });
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const deleteOpinion = async (id: string) => {
+  try {
+    const res = await axiosInstance.delete("opinions/" + id);
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+
+export const fetchOpinions = async ({
+  per_page = "10",
+  page = "1",
+  discussionId = "",
+  order = "asc",
+  authorId = "",
+}: OpinionParameters) => {
+  try {
+    const res = await axiosInstance.get(
+      `opinions?per_page=${per_page}&page=${page}&discussion-id=${discussionId}&order=${order}&author-id=${authorId}`
+    );
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const fetchOpinionById = async (id: string) => {
+  try {
+    const res = await axiosInstance.get(`opinions/${id}`);
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
---
+++ shared/services/newsApi.ts
@@ -0,0 +1,245 @@
+import axios from "axios";
+import { axiosInstance } from "./axiosInstance";
+import { News, Comment } from "@/types";
+
+export interface NewsParameters {
+  per_page: string;
+  page: string;
+  search: string;
+  sortBy: "title" | "date";
+  order: "asc" | "desc";
+}
+export interface CommentParameters {
+  per_page: string;
+  page: string;
+  newsId: string;
+  order: "asc" | "desc";
+  authorId: string;
+}
+
+export const createNews = async (dto: {
+  title: string;
+  content: string;
+  imageUrl?: string[];
+}) => {
+  try {
+    const res = await axiosInstance.post("news", { ...dto });
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+  }
+};
+export const updateNews = async (dto: Partial<News>) => {
+  try {
+    const res = await axiosInstance.patch("news", { ...dto });
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const deleteNews = async (id: string) => {
+  try {
+    const res = await axiosInstance.delete(`news/${id}`);
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+
+export const fetchNews = async ({
+  per_page = "10",
+  page = "1",
+  search = "",
+  sortBy = "title",
+  order = "asc",
+}: NewsParameters) => {
+  try {
+    const response = await axiosInstance.get(
+      `news?per_page=${per_page}&page=${page}&search=${search}&sortBy=${sortBy}&order=${order}`
+    );
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const fetchNewsById = async (id: string) => {
+  try {
+    const response = await axiosInstance.get(`news/${id}`);
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+
+export const createComment = async (dto: {
+  newsId: string;
+  content: string;
+}) => {
+  try {
+    const res = await axiosInstance.post("comments", { ...dto });
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+  }
+};
+export const updateComment = async (dto: Partial<Comment>) => {
+  try {
+    const res = await axiosInstance.patch("comments", { ...dto });
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const deleteComment = async (id: string) => {
+  try {
+    const res = await axiosInstance.delete(`comments/${id}`);
+    return {
+      statusCode: res.status,
+      data: res.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+
+export const fetchComments = async ({
+  per_page = "10",
+  page = "1",
+  newsId = "",
+  authorId = "",
+  order = "asc",
+}: CommentParameters) => {
+  try {
+    const response = await axiosInstance.get(
+      `comments?per_page=${per_page}&page=${page}&newsId=${newsId}&author-id=${authorId}&order=${order}`
+    );
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
+export const fetchCommentById = async (id: string) => {
+  try {
+    const response = await axiosInstance.get(`comments/${id}`);
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        statusCode: error.response?.status || 500,
+        error: error.response?.data?.message || "Oops, something went wrong.",
+      };
+    }
+
+    return {
+      statusCode: 500,
+      error: "Opps, something went wrong.",
+    };
+  }
+};
