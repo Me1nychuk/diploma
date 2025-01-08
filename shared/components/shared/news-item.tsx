5f157b6dddcbe365647a76e1dc01efa2833c269a
Sviatoslav <svyatoslav200104@gmail.com>
2025-01-08 15:55:40 +0200

    Add minor changes News Page

--- shared/components/shared/news-item.tsx
+++ shared/components/shared/news-item.tsx
@@ -1,19 +1,18 @@
 import { cn } from "@/shared/lib/utils";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 interface NewsItemProps {
   className?: string;
   title: string;
-  imageUrl?: string;
+
   id: string;
   description?: string;
 }
 export const NewsItem = ({
   className,
   title,
-  imageUrl,
+
   id,
   description,
 }: NewsItemProps) => {
@@ -21,30 +20,17 @@ export const NewsItem = ({
     <>
       <div
         className={cn(
-          " flex flex-col gap-1 bg-background p-2 rounded-xl custom-shadow hover:scale-105 transition-all duration-200 ]",
+          " flex flex-col gap-1 glass-2 p-2 rounded-xl  hover:scale-105 transition-all duration-200 ]",
           className
         )}
       >
-        {imageUrl ? (
-          <Image
-            src={imageUrl}
-            alt={title}
-            width={200}
-            height={200}
-            className="rounded-xl mx-auto min-md:w-[150px] min-md:h-[150px]"
-          />
-        ) : (
-          <Image
-            src={"/lion/14.png"}
-            alt={title}
-            width={200}
-            height={200}
-            className="rounded-xl mx-auto  min-md:w-[150px] min-md:h-[150px]"
-          />
+        {title && (
+          <h3 className="font-bold mb-auto">
+            {title.length > 60 ? title.slice(0, 60) + "..." : title}{" "}
+          </h3>
         )}
-        <h3 className="font-bold mb-auto">{title}</h3>
         {description && (
-          <p className=" mb-3 ">
+          <p className=" mb-3 text-sm ">
             {description?.length > 100
               ? description.slice(0, 100) + "..."
               : description}
@@ -52,9 +38,9 @@ export const NewsItem = ({
         )}
         <Link
           href={`/news/${id}`}
-          className="block bg-tertiary rounded-[5px] p-2 text-background hover:opacity-60 transition-all duration-200  self-center"
+          className="block w-full    text-tertiary hover:underline  transition-all duration-200  text-center"
         >
-          Детальніше
+          Переглянути новину
         </Link>
       </div>
     </>
