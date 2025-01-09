40dc4cee4fe3bfb9285dbebf510b000e4bd530ae
Sviatoslav <svyatoslav200104@gmail.com>
2025-01-09 15:29:14 +0200

    Update page.tsx

--- app/(auth)/google/success/page.tsx
+++ app/(auth)/google/success/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 import { GoogleContent } from "@/shared/components/shared/auth";
-import { Loader2 } from "lucide-react";
 import { useSearchParams } from "next/navigation";
-import React, { Suspense } from "react";
+import React from "react";
 
 const Page: React.FC = () => {
-  const token = useSearchParams().get("token");
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get("token");
   return (
     <div className="w-full max-w-[700px] p-5 rounded-xl glass h-full mx-auto">
-      <Suspense fallback={<Loader2 className="animate-spin mx-auto mt-2" />}>
-        {token && <GoogleContent token={token} />}
-      </Suspense>
+      {token && <GoogleContent token={token} />}
     </div>
   );
 };
