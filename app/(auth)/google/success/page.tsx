360ffe9b8bbb5ac8fdb611768359faa77205c708
Sviatoslav <svyatoslav200104@gmail.com>
2025-01-09 14:52:10 +0200

    update the logic of receiving a token

--- app/(auth)/google/success/page.tsx
+++ app/(auth)/google/success/page.tsx
@@ -1,11 +1,11 @@
 import { GoogleContent } from "@/shared/components/shared/auth";
 import { Loader2 } from "lucide-react";
-import { cookies } from "next/headers";
 import React, { Suspense } from "react";
 
 const Page: React.FC = () => {
-  const cookieStore = cookies();
-  const token = cookieStore.get("googleToken")?.value;
+  const searchParams = new URLSearchParams(window.location.search);
+  const token = searchParams.get("token");
+
   return (
     <div className="w-full max-w-[700px] p-5 rounded-xl glass h-full mx-auto">
       <Suspense fallback={<Loader2 className="animate-spin mx-auto mt-2" />}>
