b2137dfa2169d8fdbe1e20a759cde9b382d00275
Sviatoslav <svyatoslav200104@gmail.com>
2025-01-27 15:58:47 +0200

    Add minor fix(correct type)

--- app/(dashboard)/admin/layout.tsx
+++ app/(dashboard)/admin/layout.tsx
@@ -8,18 +8,13 @@ export const metadata: Metadata = {
 
 export default function HomeLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <>
       <Container className=" relative  flex-1 glass max-w-[1400px]  max-h-full rounded-2xl px-4 py-3 ">
-        <main>
-          {modal}
-          {children}
-        </main>
+        <main>{children}</main>
       </Container>
     </>
   );
